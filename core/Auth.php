191d4efb80c2d28bb2ee7ef80c0ddc7d792864ea
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-09-22 13:31:18 -0600

    feature: Adds user() method to retrieve a instance from __auth values in $_SESSION.

--- core/Auth.php
+++ core/Auth.php
@@ -66,4 +66,33 @@ class Auth {
     public static function authenticated(): bool {
         return isset($_SESSION['___auth']);
     }
+
+    /**
+     *  Get current __auth values in $_SESSION.
+     *
+     * @return array | null An array with all values or null if __auth is not declared or has a empty value.
+     */
+    private static function getAuth(): array | null{
+        return Session::get('__auth');
+    }
+
+        /**
+     *  Try to return a instance of the user model from current $_SESSION[__auth] id value.
+     *
+     * @param string $modelClassName The classname of the model to retrieve.
+     * @return object|null a fresh instance of the model or null if there's nothing in current __auth $_SESSION.
+     */
+    public static function user(string $modelClassName): object | null{
+        $auth = static::getAuth();
+        if($auth === null) return null;
+        $user = new $modelClassName();
+
+        if(!method_exists($user, "find")) return null;
+
+        $id = JustArray::find($auth, "id") ?? null;
+
+        if($id === null) return null;
+
+        return $user->find($id);
+    }
 }
